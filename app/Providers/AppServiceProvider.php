bcbe4d1f28c537f7400d76bf8a8f219896edc0f2
yuzu <yuzu_15230228@yahoo.co.jp>
2020-06-07 07:30:37 +0900

    4th commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if (request()->isSecure()) {
+            \URL::forceScheme('https');
+        }
     }
 }
